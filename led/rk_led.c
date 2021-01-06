829852b81a5b3678508eddc88bc10bdacd639315
IfanTsai <1558499691@qq.com>
2021-01-07 01:38:01 +0800

    clean up code

--- led/rk_led.c
+++ led/rk_led.c
@@ -14,228 +14,228 @@ static struct class *led_class;
 #define LED_ON_LEVEL    (1)
 #define LED_OFF_LEVEL   (0)
 
-#define LED_MAGIC      'c'
-#define IOCTL_LED_ON    _IOW(LED_MAGIC, 1, int)
-#define IOCTL_LED_OFF   _IOW(LED_MAGIC, 2, int)
+#define LED_MAGIC                  'c'
+#define IOCTL_LED_ON               _IO(LED_MAGIC, 1)
+#define IOCTL_LED_OFF              _IO(LED_MAGIC, 2)
 #define IOCTL_LED_SET_SHINE_CNT    _IOW(LED_MAGIC, 3, int)
 
 struct led_data {
-	unsigned int led_gpio;
-	const char *led_name;
-	dev_t led_dev;
-	struct cdev led_cdev;
+    unsigned int led_gpio;
+    const char *led_name;
+    dev_t led_dev;
+    struct cdev led_cdev;
 };
 
 static long led_ioctl(struct file *file, unsigned int cmd, unsigned long cnt)
 {
-	struct led_data *led_data = file->private_data;
+    struct led_data *led_data = file->private_data;
 
-	switch (cmd) {
-	case IOCTL_LED_ON:
-		gpio_set_value(led_data->led_gpio, LED_ON_LEVEL);
-		pr_err("====> %s: led_ioctl on!\n", led_data->led_name);
-		break;
-	case IOCTL_LED_OFF:
-		gpio_set_value(led_data->led_gpio, LED_OFF_LEVEL);
-		pr_err("====> %s: led_ioctl off!\n", led_data->led_name);
-		break;
-	case IOCTL_LED_SET_SHINE_CNT:
-	{
-		int i, val = gpio_get_value(led_data->led_gpio);
+    switch (cmd) {
+    case IOCTL_LED_ON:
+        gpio_set_value(led_data->led_gpio, LED_ON_LEVEL);
+        pr_err("====> %s: led_ioctl on!\n", led_data->led_name);
+        break;
+    case IOCTL_LED_OFF:
+        gpio_set_value(led_data->led_gpio, LED_OFF_LEVEL);
+        pr_err("====> %s: led_ioctl off!\n", led_data->led_name);
+        break;
+    case IOCTL_LED_SET_SHINE_CNT:
+    {
+        int i, val = gpio_get_value(led_data->led_gpio);
 
-		pr_err("====> %s: led_ioctl set shine count %ld!\n", led_data->led_name, cnt);
-		for (i = 0; i < cnt; i++) {
-			gpio_set_value(led_data->led_gpio, !val);
-			mdelay(500);
-			gpio_set_value(led_data->led_gpio, val);
-			mdelay(500);
-		}
-		break;
-	}
-	default:
-		break;
-	}
-	return 0;
+        pr_err("====> %s: led_ioctl set shine count %ld!\n", led_data->led_name, cnt);
+        for (i = 0; i < cnt; i++) {
+            gpio_set_value(led_data->led_gpio, !val);
+            mdelay(500);
+            gpio_set_value(led_data->led_gpio, val);
+            mdelay(500);
+        }
+        break;
+    }
+    default:
+        break;
+    }
+    return 0;
 }
 
 static ssize_t led_read(struct file *file, char __user *ubuf,
-		size_t count, loff_t *offset)
+        size_t count, loff_t *offset)
 {
-	int val;
-	char kbuf[8] = { 0 };
-	struct led_data *led_data = file->private_data;
+    int val;
+    char kbuf[8] = { 0 };
+    struct led_data *led_data = file->private_data;
 
-	gpio_direction_input(led_data->led_gpio);
-	val = gpio_get_value(led_data->led_gpio);
-	gpio_direction_output(led_data->led_gpio, val);
-	
-	if (*offset >= sizeof(min(sizeof(kbuf), count)))
-		return 0;
+    gpio_direction_input(led_data->led_gpio);
+    val = gpio_get_value(led_data->led_gpio);
+    gpio_direction_output(led_data->led_gpio, val);
 
-	sprintf(kbuf, "%d", val);
-	if (copy_to_user(ubuf, kbuf, min(sizeof(kbuf), count))) {
-		pr_err("copy_to_user error!!!\n");
-		return -EINVAL;
-	}
-	return min(sizeof(kbuf), count);
+    if (*offset >= min(sizeof(kbuf), count))
+        return 0;
+
+    sprintf(kbuf, "%d", val);
+    if (copy_to_user(ubuf, kbuf, min(sizeof(kbuf), count))) {
+        pr_err("copy_to_user error!!!\n");
+        return -EINVAL;
+    }
+    return min(sizeof(kbuf), count);
 }
 
 static ssize_t led_write(struct file *file, const char __user *ubuf,
-		size_t count, loff_t *offset)
+        size_t count, loff_t *offset)
 {
-	int val;
-	char kbuf[8] = { 0 };
-	struct led_data *led_data = file->private_data;
+    int val;
+    char kbuf[8] = { 0 };
+    struct led_data *led_data = file->private_data;
 
-	if (copy_from_user(kbuf, ubuf, min(sizeof(kbuf), count))) {
-		pr_err("copy_from_user error!!!\n");
-		return -EINVAL;
-	}
-	if (kstrtoint(kbuf, 10, &val) < 0) {
-		pr_err("kstrtoint error!\n");
-		return -EINVAL;
-	}
-	pr_err("====> %s: val = %d\n", led_data->led_name, val);
-	gpio_set_value(led_data->led_gpio, val);
-	return min(sizeof(kbuf), count);
+    if (copy_from_user(kbuf, ubuf, min(sizeof(kbuf), count))) {
+        pr_err("copy_from_user error!!!\n");
+        return -EINVAL;
+    }
+    if (kstrtoint(kbuf, 10, &val) < 0) {
+        pr_err("kstrtoint error!\n");
+        return -EINVAL;
+    }
+    pr_err("====> %s: val = %d\n", led_data->led_name, val);
+    gpio_set_value(led_data->led_gpio, val);
+    return min(sizeof(kbuf), count);
 }
 
 static int led_open(struct inode *inode, struct file *file)
 {
-	struct led_data *led_data;
+    struct led_data *led_data;
 
-	led_data = container_of(inode->i_cdev, struct led_data, led_cdev);
-	file->private_data = led_data;
-	return 0;
+    led_data = container_of(inode->i_cdev, struct led_data, led_cdev);
+    file->private_data = led_data;
+    return 0;
 }
 
 static int led_release(struct inode *inode, struct file *file)
 {
-	return 0;
+    return 0;
 }
 
 static const struct file_operations led_fops = {
-	.owner		        = THIS_MODULE,
-	.open		        = led_open,
-	.release	        = led_release,
-	.read	            = led_read,
-	.write              = led_write,
-	.unlocked_ioctl     = led_ioctl,
-	.compat_ioctl       = led_ioctl,
+    .owner                = THIS_MODULE,
+    .open                = led_open,
+    .release            = led_release,
+    .read                = led_read,
+    .write              = led_write,
+    .unlocked_ioctl     = led_ioctl,
+    .compat_ioctl       = led_ioctl,
 };
 
 static int led_probe(struct platform_device *pdev)
 {
-	int ret;
-	unsigned int led_gpio;
-	enum of_gpio_flags flag;
-	const char *led_name;
-	dev_t led_dev;
-	struct led_data *led_data = NULL;
-	struct device_node *np = pdev->dev.of_node;
+    int ret;
+    unsigned int led_gpio;
+    enum of_gpio_flags flag;
+    const char *led_name;
+    dev_t led_dev;
+    struct led_data *led_data = NULL;
+    struct device_node *np = pdev->dev.of_node;
 
-	of_property_read_string(np, "led_name", &led_name);
+    of_property_read_string(np, "led_name", &led_name);
 
-	led_gpio = of_get_named_gpio_flags(np, "led_gpio", 0, &flag);
-	if (!gpio_is_valid(led_gpio)) {
-		pr_err("%s: led-gpio %d is invalid\n", led_name, led_gpio);
-		return -ENODEV;
-	}
-	if (gpio_request(led_gpio, led_name)) {
-		pr_err("%s: gpio %d request failed!\n", led_name, led_gpio);
-		return -ENODEV;
-	}
-	gpio_direction_output(led_gpio, (flag == OF_GPIO_ACTIVE_LOW) ? 0 : 1);
+    led_gpio = of_get_named_gpio_flags(np, "led_gpio", 0, &flag);
+    if (!gpio_is_valid(led_gpio)) {
+        pr_err("%s: led-gpio %d is invalid\n", led_name, led_gpio);
+        return -ENODEV;
+    }
+    if (gpio_request(led_gpio, led_name)) {
+        pr_err("%s: gpio %d request failed!\n", led_name, led_gpio);
+        return -ENODEV;
+    }
+    gpio_direction_output(led_gpio, (flag == OF_GPIO_ACTIVE_LOW) ? 0 : 1);
 
-	ret = alloc_chrdev_region(&led_dev, 0, 1, led_name);
-	if (ret < 0) {
-		pr_err("register_chrdev %s failed!\n", led_name);
-		goto out_alloc_chrdev;
-	}
+    ret = alloc_chrdev_region(&led_dev, 0, 1, led_name);
+    if (ret < 0) {
+        pr_err("register_chrdev %s failed!\n", led_name);
+        goto out_alloc_chrdev;
+    }
 
-	led_data = kzalloc(sizeof(*led_data), GFP_KERNEL);
-	if (!led_data) {
-		pr_err("could not allocate led_data!\n");
-		goto out_kzalloc;
-	}
+    led_data = kzalloc(sizeof(*led_data), GFP_KERNEL);
+    if (!led_data) {
+        pr_err("could not allocate led_data!\n");
+        goto out_kzalloc;
+    }
 
-	cdev_init(&led_data->led_cdev, &led_fops);
-	led_data->led_cdev.owner = THIS_MODULE;
-	ret = cdev_add(&led_data->led_cdev, led_dev, 1);
-	if (ret < 0) {
-		pr_err("%s cdev_add error!\n", led_name);
-		goto out_cdev_add;
-	}
+    cdev_init(&led_data->led_cdev, &led_fops);
+    led_data->led_cdev.owner = THIS_MODULE;
+    ret = cdev_add(&led_data->led_cdev, led_dev, 1);
+    if (ret < 0) {
+        pr_err("%s cdev_add error!\n", led_name);
+        goto out_cdev_add;
+    }
 
-	device_create(led_class, NULL, led_dev, NULL, led_name);
+    device_create(led_class, NULL, led_dev, NULL, led_name);
 
-	led_data->led_dev  = led_dev;
-	led_data->led_gpio = led_gpio;
-	led_data->led_name = led_name;
-	platform_set_drvdata(pdev, led_data);
+    led_data->led_dev  = led_dev;
+    led_data->led_gpio = led_gpio;
+    led_data->led_name = led_name;
+    platform_set_drvdata(pdev, led_data);
 
-	pr_err("=========> %s probe success!\n", led_data->led_name);
-	return 0;
+    pr_err("=========> %s probe success!\n", led_data->led_name);
+    return 0;
 
 out_cdev_add:
-	kfree(led_data);
+    kfree(led_data);
 out_kzalloc:
-	unregister_chrdev_region(led_dev, 1);
+    unregister_chrdev_region(led_dev, 1);
 out_alloc_chrdev:
-	gpio_free(led_gpio);
-	return -EINVAL;
+    gpio_free(led_gpio);
+    return -EINVAL;
 }
 
 static int led_remove(struct platform_device *pdev)
 {
-	struct led_data *led_data = platform_get_drvdata(pdev);
+    struct led_data *led_data = platform_get_drvdata(pdev);
 
-	device_destroy(led_class, led_data->led_dev);
-	cdev_del(&led_data->led_cdev);
-	unregister_chrdev_region(led_data->led_dev, 1);
-	gpio_free(led_data->led_gpio);
-	kfree(led_data);
+    device_destroy(led_class, led_data->led_dev);
+    cdev_del(&led_data->led_cdev);
+    unregister_chrdev_region(led_data->led_dev, 1);
+    gpio_free(led_data->led_gpio);
+    kfree(led_data);
 
-	pr_err("=========> %s remove success!\n", led_data->led_name);
-	return 0;
+    pr_err("=========> %s remove success!\n", led_data->led_name);
+    return 0;
 }
 
 static const struct of_device_id rk_led_of_match[] = {
-	{ .compatible = "rockchip,led_red", },
-	{ .compatible = "rockchip,led_green", },
-	{ .compatible = "rockchip,led_yellow", },
-	{ },
+    { .compatible = "rockchip,led_red", },
+    { .compatible = "rockchip,led_green", },
+    { .compatible = "rockchip,led_yellow", },
+    { },
 };
 
 static struct platform_driver rk_led = {
-	.probe = led_probe,
-	.remove = led_remove,
-	.driver = {
-		.name = "rk_led",
-		.owner = THIS_MODULE,
-		.of_match_table = rk_led_of_match,
-	}
+    .probe = led_probe,
+    .remove = led_remove,
+    .driver = {
+        .name = "rk_led",
+        .owner = THIS_MODULE,
+        .of_match_table = rk_led_of_match,
+    }
 };
 
 static int __init rk_led_init(void)
 {
-	led_class = class_create(THIS_MODULE, LED_CLASS);
-	if (IS_ERR(led_class)) {
-		pr_err("class create %s failed!\n", LED_CLASS);
-		return -EINVAL;
-	}
-	return platform_driver_register(&rk_led);
+    led_class = class_create(THIS_MODULE, LED_CLASS);
+    if (IS_ERR(led_class)) {
+        pr_err("class create %s failed!\n", LED_CLASS);
+        return -EINVAL;
+    }
+    return platform_driver_register(&rk_led);
 }
 
 static void __exit rk_led_exit(void)
 {
-	platform_driver_unregister(&rk_led);
-	class_destroy(led_class);
+    platform_driver_unregister(&rk_led);
+    class_destroy(led_class);
 }
 
 module_init(rk_led_init);
 module_exit(rk_led_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Ifan Tsai");
+MODULE_AUTHOR("Ifan Tsai <i@caiyifan.cn>");
 MODULE_DESCRIPTION("led driver for rk3399");
--- timer/rk_timer.c
+++ timer/rk_timer.c
@@ -16,34 +16,35 @@
 #include <linux/of.h>
 #include <linux/of_address.h>
 
-#define RK_DEV_MAX   1
+#define RK_DEV_MAX   (1)
 
-#define RK_TIMER_START           (0x01)
-#define RK_TIMER_STOP            (0x02)
-#define RK_TIMER_SET_INTERVAL    (0x03)
+#define RK_TIMER_MAGIC           't'
+#define RK_TIMER_START           _IO(RK_TIMER_MAGIC, 0x01)
+#define RK_TIMER_STOP            _IO(RK_TIMER_MAGIC, 0x02)
+#define RK_TIMER_SET_INTERVAL    _IOW(RK_TIMER_MAGIC, 0x03, unsigned int)
 
 struct rk_timer_reg {
-	unsigned int load_cnt0;
-	unsigned int load_cnt1;
-	unsigned int curr_val0;
-	unsigned int curr_val1;
-	unsigned int load_cnt2;
-	unsigned int load_cnt3;
-	unsigned int stat;
-	unsigned int ctlreg;
+    unsigned int load_cnt0;
+    unsigned int load_cnt1;
+    unsigned int curr_val0;
+    unsigned int curr_val1;
+    unsigned int load_cnt2;
+    unsigned int load_cnt3;
+    unsigned int stat;
+    unsigned int ctlreg;
 };
 
 struct rk_timer {
-	struct miscdevice miscdev;
-	int irq;
-	unsigned long interval;
-	bool done;
-	unsigned long dev_opened;
-	/* timer->done */
-	spinlock_t lock;
-	struct rk_timer_reg *reg;
-	struct clk *timer_clk;
-	struct clk *pclk;
+    struct miscdevice miscdev;
+    int irq;
+    unsigned long interval;
+    bool done;
+    unsigned long dev_opened;
+    /* timer->done */
+    spinlock_t lock;
+    struct rk_timer_reg *reg;
+    struct clk *timer_clk;
+    struct clk *pclk;
 };
 
 static void __iomem *timer_base;
@@ -54,328 +55,328 @@ DECLARE_WAIT_QUEUE_HEAD(rk_timer_wq);
 
 static irqreturn_t rk_timer_interrupt(int irq, void *dev_id)
 {
-	unsigned long flags;
+    unsigned long flags;
 
-	spin_lock_irqsave(&g_ptimer->lock, flags);
-	/* clear INTSTATUS */
-	writel(1, &g_ptimer->reg->stat);
-	g_ptimer->done = true;
-	wake_up(&rk_timer_wq);
+    spin_lock_irqsave(&g_ptimer->lock, flags);
+    /* clear INTSTATUS */
+    writel(1, &g_ptimer->reg->stat);
+    g_ptimer->done = true;
+    wake_up(&rk_timer_wq);
 
-	spin_unlock_irqrestore(&g_ptimer->lock, flags);
+    spin_unlock_irqrestore(&g_ptimer->lock, flags);
 
-	return IRQ_HANDLED;
+    return IRQ_HANDLED;
 }
 
 static void timer_set_interval(struct rk_timer *timer, u64 us)
 {
-	struct rk_timer_reg *timer_reg = timer->reg;
-	u64 count;
+    struct rk_timer_reg *timer_reg = timer->reg;
+    u64 count;
 
-	if (us >= 1000 && us <= 10000000) {      /* allow: 1ms ~ 10s */
-		/* clock: 24MHz */
-		/* period : 1.0 / 24000000 * 1000000 = 1 / 24 us */
-		count = 24 * us;
+    if (us >= 1000 && us <= 10000000) {      /* allow: 1ms ~ 10s */
+        /* clock: 24MHz */
+        /* period : 1.0 / 24000000 * 1000000 = 1 / 24 us */
+        count = 24 * us;
 
-		/* load count2,3 val */
-		writel(0, &timer_reg->load_cnt2);
-		writel(0, &timer_reg->load_cnt3);
-		/* load count1,2 val */
-		writel(count & 0xFFFFFFFF, &timer_reg->load_cnt0);
-		writel(count >> 32, &timer_reg->load_cnt1);
-	}
+        /* load count2,3 val */
+        writel(0, &timer_reg->load_cnt2);
+        writel(0, &timer_reg->load_cnt3);
+        /* load count1,2 val */
+        writel(count & 0xFFFFFFFF, &timer_reg->load_cnt0);
+        writel(count >> 32, &timer_reg->load_cnt1);
+    }
 }
 
 static void timer_enable(struct rk_timer *timer)
 {
-	struct rk_timer_reg *timer_reg = timer->reg;
-	unsigned int val;
+    struct rk_timer_reg *timer_reg = timer->reg;
+    unsigned int val;
 
-	val = readl(&timer_reg->ctlreg);
-	/* timer interrupt not mask */
-	val |= (0x01 << 2);
-	/* enable timer */
-	val |= (0x01 << 0);
-	writel(val, &timer_reg->ctlreg);
+    val = readl(&timer_reg->ctlreg);
+    /* timer interrupt not mask */
+    val |= (0x01 << 2);
+    /* enable timer */
+    val |= (0x01 << 0);
+    writel(val, &timer_reg->ctlreg);
 }
 
 static void timer_disable(struct rk_timer *timer)
 {
-	struct rk_timer_reg *timer_reg = timer->reg;
-	unsigned int val = readl(&timer_reg->ctlreg);
+    struct rk_timer_reg *timer_reg = timer->reg;
+    unsigned int val = readl(&timer_reg->ctlreg);
 
-	val = readl(&timer_reg->ctlreg);
-	/* timer interrupt mask */
-	val &= ~(0x01 << 2);
-	/* disable timer */
-	val &= ~(0x01 << 0);
-	writel(val, &timer_reg->ctlreg);
+    val = readl(&timer_reg->ctlreg);
+    /* timer interrupt mask */
+    val &= ~(0x01 << 2);
+    /* disable timer */
+    val &= ~(0x01 << 0);
+    writel(val, &timer_reg->ctlreg);
 }
 
 static void timer_init(struct rk_timer *timer)
 {
-	struct rk_timer_reg *timer_reg = timer->reg;
-	unsigned int val = readl(&timer_reg->ctlreg);
+    struct rk_timer_reg *timer_reg = timer->reg;
+    unsigned int val = readl(&timer_reg->ctlreg);
 
-	/* disable timer */
-	val &= ~(0x01 << 0);
-	/* set timer mode: free-running */
-	val &= ~(0x01 << 1);
-	/* timer interrupt mask */
-	val &= ~(0x01 << 2);
+    /* disable timer */
+    val &= ~(0x01 << 0);
+    /* set timer mode: free-running */
+    val &= ~(0x01 << 1);
+    /* timer interrupt mask */
+    val &= ~(0x01 << 2);
 
-	writel(val, &timer_reg->ctlreg);
+    writel(val, &timer_reg->ctlreg);
 
-	timer_set_interval(timer, 10000);      /* defautl: 10ms */
-	timer->interval = 10000;
+    timer_set_interval(timer, 10000);      /* defautl: 10ms */
+    timer->interval = 10000;
 }
 
 static int rk_timer_open(struct inode *inode, struct file *file)
 {
-	struct rk_timer *timer;
+    struct rk_timer *timer;
 
-	timer = container_of(file->private_data, struct rk_timer, miscdev);
+    timer = container_of(file->private_data, struct rk_timer, miscdev);
 
-	if (test_and_set_bit(0, &timer->dev_opened))
-		return -EBUSY;
+    if (test_and_set_bit(0, &timer->dev_opened))
+        return -EBUSY;
 
-	return 0;
+    return 0;
 }
 
 static int rk_timer_release(struct inode *inode, struct file *file)
 {
-	struct rk_timer *timer;
+    struct rk_timer *timer;
 
-	timer = container_of(file->private_data, struct rk_timer, miscdev);
+    timer = container_of(file->private_data, struct rk_timer, miscdev);
 
-	clear_bit(0, &timer->dev_opened);
-	timer_disable(timer);
+    clear_bit(0, &timer->dev_opened);
+    timer_disable(timer);
 
-	return 0;
+    return 0;
 }
 
 static unsigned int rk_timer_poll(struct file *file, poll_table *wait)
 {
-	struct rk_timer *timer;
-	unsigned int ret;
-	unsigned long flags;
+    struct rk_timer *timer;
+    unsigned int ret;
+    unsigned long flags;
 
-	timer = container_of(file->private_data, struct rk_timer, miscdev);
+    timer = container_of(file->private_data, struct rk_timer, miscdev);
 
-	poll_wait(file, &rk_timer_wq, wait);
+    poll_wait(file, &rk_timer_wq, wait);
 
-	spin_lock_irqsave(&timer->lock, flags);
-	if (timer->done) {
-		ret = POLLIN | POLLRDNORM;
-		timer->done = false;
-	} else {
-		ret = 0;
-	}
-	spin_unlock_irqrestore(&timer->lock, flags);
+    spin_lock_irqsave(&timer->lock, flags);
+    if (timer->done) {
+        ret = POLLIN | POLLRDNORM;
+        timer->done = false;
+    } else {
+        ret = 0;
+    }
+    spin_unlock_irqrestore(&timer->lock, flags);
 
-	return ret;
+    return ret;
 }
 
 static long rk_timer_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
-	struct rk_timer *timer;
+    struct rk_timer *timer;
 
-	timer = container_of(file->private_data, struct rk_timer, miscdev);
+    timer = container_of(file->private_data, struct rk_timer, miscdev);
 
-	switch (cmd) {
-		case RK_TIMER_START:
-			timer_enable(timer);
-			break;
-		case RK_TIMER_STOP:
-			timer_disable(timer);
-			break;
-		case RK_TIMER_SET_INTERVAL:
-		{
-			unsigned int val;
+    switch (cmd) {
+        case RK_TIMER_START:
+            timer_enable(timer);
+            break;
+        case RK_TIMER_STOP:
+            timer_disable(timer);
+            break;
+        case RK_TIMER_SET_INTERVAL:
+        {
+            unsigned int val;
 
-			if (copy_from_user(&val, (void __user *)arg, sizeof(int)))
-				return -EFAULT;
+            if (copy_from_user(&val, (void __user *)arg, sizeof(int)))
+                return -EFAULT;
 
-			timer->interval = val;
-			timer_set_interval(timer, timer->interval);
-			break;
-		}
-		default:
-			return -ENOTTY;
-	}
+            timer->interval = val;
+            timer_set_interval(timer, timer->interval);
+            break;
+        }
+        default:
+            return -ENOTTY;
+    }
 
-	return 0;
+    return 0;
 }
 
 static const struct file_operations rk_timer_fops = {
-	.owner   = THIS_MODULE,
-	.llseek  = no_llseek,
-	.open    = rk_timer_open,
-	.release = rk_timer_release,
-	.poll    = rk_timer_poll,
-	.unlocked_ioctl = rk_timer_ioctl,
-	.compat_ioctl   = rk_timer_ioctl,
+    .owner   = THIS_MODULE,
+    .llseek  = no_llseek,
+    .open    = rk_timer_open,
+    .release = rk_timer_release,
+    .poll    = rk_timer_poll,
+    .unlocked_ioctl = rk_timer_ioctl,
+    .compat_ioctl   = rk_timer_ioctl,
 };
 
 static int rk_timer_remove(struct platform_device *pdev)
 {
-	struct rk_timer *timer = platform_get_drvdata(pdev);
+    struct rk_timer *timer = platform_get_drvdata(pdev);
 
-	free_irq(timer->irq, NULL);
-	misc_deregister(&timer->miscdev);
-	clk_disable_unprepare(timer->timer_clk);
-	clk_disable_unprepare(timer->pclk);
-	iounmap(timer->reg);
-	kfree(timer);
+    free_irq(timer->irq, NULL);
+    misc_deregister(&timer->miscdev);
+    clk_disable_unprepare(timer->timer_clk);
+    clk_disable_unprepare(timer->pclk);
+    iounmap(timer->reg);
+    kfree(timer);
 
-	return 0;
+    return 0;
 }
 
 static int rk_timer_probe(struct platform_device *pdev)
 {
-	struct rk_timer *timer;
-	int err, irq;
-	struct clk *timer_clk;
-	struct clk *pclk;
-	struct rk_timer_reg *timer_reg;
+    struct rk_timer *timer;
+    int err, irq;
+    struct clk *timer_clk;
+    struct clk *pclk;
+    struct rk_timer_reg *timer_reg;
 
-	struct device_node *np = pdev->dev.of_node;
+    struct device_node *np = pdev->dev.of_node;
 
-	timer_base = of_iomap(np, 0);
-	if (!timer_base) {
-		pr_err("Failed to get base address for rk_timer2\n");
-		return -EINVAL;
-	}
-	timer_reg = (struct rk_timer_reg *)timer_base;
+    timer_base = of_iomap(np, 0);
+    if (!timer_base) {
+        pr_err("Failed to get base address for rk_timer2\n");
+        return -EINVAL;
+    }
+    timer_reg = (struct rk_timer_reg *)timer_base;
 
-	pclk = of_clk_get_by_name(np, "pclk");
-	if (IS_ERR(pclk)) {
-		pr_err("Failed to get pclk for rk_timer2\n");
-		return -EINVAL;
-	}
+    pclk = of_clk_get_by_name(np, "pclk");
+    if (IS_ERR(pclk)) {
+        pr_err("Failed to get pclk for rk_timer2\n");
+        return -EINVAL;
+    }
 
-	if (clk_prepare_enable(pclk)) {
-		pr_err("Failed to enable pclk for rk_timer2\n");
-		return -EINVAL;
-	}
+    if (clk_prepare_enable(pclk)) {
+        pr_err("Failed to enable pclk for rk_timer2\n");
+        return -EINVAL;
+    }
 
-	timer_clk = of_clk_get_by_name(np, "timer");
-	if (IS_ERR(timer_clk)) {
-		pr_err("Failed to get timer clock for rk_timer2\n");
-		return -EINVAL;
-	}
+    timer_clk = of_clk_get_by_name(np, "timer");
+    if (IS_ERR(timer_clk)) {
+        pr_err("Failed to get timer clock for rk_timer2\n");
+        return -EINVAL;
+    }
 
-	if (clk_prepare_enable(timer_clk)) {
-		pr_err("Failed to enable timer clock\n");
-		return -EINVAL;
-	}
+    if (clk_prepare_enable(timer_clk)) {
+        pr_err("Failed to enable timer clock\n");
+        return -EINVAL;
+    }
 
-	irq = irq_of_parse_and_map(np, 0);
-	if (!irq) {
-		pr_err("Failed to map interrupts for rk_timer2\n");
-		return -EINVAL;
-	}
+    irq = irq_of_parse_and_map(np, 0);
+    if (!irq) {
+        pr_err("Failed to map interrupts for rk_timer2\n");
+        return -EINVAL;
+    }
 
-	timer = kzalloc(sizeof(*timer), GFP_KERNEL);
-	if (!timer) {
-		err = -ENOMEM;
-		goto err_return;
-	}
+    timer = kzalloc(sizeof(*timer), GFP_KERNEL);
+    if (!timer) {
+        err = -ENOMEM;
+        goto err_return;
+    }
 
-	spin_lock_init(&timer->lock);
+    spin_lock_init(&timer->lock);
 
-	timer->miscdev.minor = MISC_DYNAMIC_MINOR;
-	timer->miscdev.name = "rk_timer2";
-	timer->miscdev.fops = &rk_timer_fops;
-	err = misc_register(&timer->miscdev);
-	if (err) {
-		pr_err("Register %s failed\n", timer->miscdev.name);
-		goto err_free_priv;
-	}
-	pr_err("misc_register success\n");
+    timer->miscdev.minor = MISC_DYNAMIC_MINOR;
+    timer->miscdev.name = "rk_timer2";
+    timer->miscdev.fops = &rk_timer_fops;
+    err = misc_register(&timer->miscdev);
+    if (err) {
+        pr_err("Register %s failed\n", timer->miscdev.name);
+        goto err_free_priv;
+    }
+    pr_err("misc_register success\n");
 
-	timer->reg = timer_reg;
-	timer->pclk = pclk;
-	timer->timer_clk = timer_clk;
+    timer->reg = timer_reg;
+    timer->pclk = pclk;
+    timer->timer_clk = timer_clk;
 
-	timer_init(timer);
+    timer_init(timer);
 
-	g_ptimer = timer;
+    g_ptimer = timer;
 
-	timer->irq = irq;
-	err = request_irq(irq, rk_timer_interrupt, IRQF_TIMER, "rk_timer2", NULL);
+    timer->irq = irq;
+    err = request_irq(irq, rk_timer_interrupt, IRQF_TIMER, "rk_timer2", NULL);
 
-	if (err < 0) {
-		pr_err("fail to request rk_timer2 irq\n");
-		goto err_misc_register;
-	}
+    if (err < 0) {
+        pr_err("fail to request rk_timer2 irq\n");
+        goto err_misc_register;
+    }
 
-	platform_set_drvdata(pdev, timer);
+    platform_set_drvdata(pdev, timer);
 
-	return 0;
+    return 0;
 
 err_misc_register:
-	misc_deregister(&timer->miscdev);
+    misc_deregister(&timer->miscdev);
 err_free_priv:
-	kfree(timer);
+    kfree(timer);
 err_return:
-	clk_disable_unprepare(timer->timer_clk);
-	clk_disable_unprepare(timer->pclk);
-	iounmap(timer->reg);
-	return err;
+    clk_disable_unprepare(timer->timer_clk);
+    clk_disable_unprepare(timer->pclk);
+    iounmap(timer->reg);
+    return err;
 }
 
 #if defined CONFIG_PM
 int rk_timer_suspend(struct device *dev)
 {
-	struct rk_timer *timer = dev_get_drvdata(dev);
+    struct rk_timer *timer = dev_get_drvdata(dev);
 
-	timer_disable(timer);
-	return 0;
+    timer_disable(timer);
+    return 0;
 }
 
 int rk_timer_resume(struct device *dev)
 {
-	struct rk_timer *timer = dev_get_drvdata(dev);
+    struct rk_timer *timer = dev_get_drvdata(dev);
 
-	timer_set_interval(timer, timer->interval);
-	if (timer->dev_opened)
-		timer_enable(timer);
-	return 0;
+    timer_set_interval(timer, timer->interval);
+    if (timer->dev_opened)
+        timer_enable(timer);
+    return 0;
 }
 
 static const struct dev_pm_ops rk_timer_pm_ops = {
-	.suspend = rk_timer_suspend,
-	.resume  = rk_timer_resume,
+    .suspend = rk_timer_suspend,
+    .resume  = rk_timer_resume,
 };
 #endif
 
 static const struct of_device_id rk_timer_of_ids[] = {
-	{ .compatible = "rockchip,rk-timer2", },
-	{}
+    { .compatible = "rockchip,rk-timer2", },
+    {}
 };
 
 static struct platform_driver rk_timer_driver = {
-	.probe = rk_timer_probe,
-	.remove = rk_timer_remove,
-	.driver = {
-		.name = "rk_timer2",
-		.owner = THIS_MODULE,
+    .probe = rk_timer_probe,
+    .remove = rk_timer_remove,
+    .driver = {
+        .name = "rk_timer2",
+        .owner = THIS_MODULE,
 #ifdef CONFIG_PM
-		.pm = &rk_timer_pm_ops,
+        .pm = &rk_timer_pm_ops,
 #endif
-		.of_match_table = rk_timer_of_ids,
-	},
+        .of_match_table = rk_timer_of_ids,
+    },
 };
 
 static int __init rk_timer_init(void)
 {
-	return platform_driver_register(&rk_timer_driver);
+    return platform_driver_register(&rk_timer_driver);
 }
 
 static void __exit rk_timer_exit(void)
 {
-	platform_driver_unregister(&rk_timer_driver);
+    platform_driver_unregister(&rk_timer_driver);
 }
 
 module_init(rk_timer_init);
